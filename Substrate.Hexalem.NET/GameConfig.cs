13b35412cb61f5d5c24fc79688c33f193f5a71c7
cedric.decoster <cedric.decoster@gmail.com>
2023-12-28 19:42:45 +0100

    Projektdateien hinzufügen.

---
+++ Substrate.Hexalem.NET/GameConfig.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace Substrate.Hexalem.Engine
+{
+    public static class GameConfig
+    {
+        public const uint MAX_TURN_BLOCKS = 10;
+
+        public const double WATER_PER_HUMANS = 2;
+        public const double FOOD_PER_HUMANS = 1;
+        public const double HOME_PER_HUMANS = 3;
+
+        public const double WATER_PER_WATER = 2;
+        public const double FOOD_PER_GRASS = 2;
+        public const double FOOD_PER_TREE = 1;
+
+        public const int GAME_STORAGE_ID = 32;
+        public const int GAME_STORAGE_SIZE = 16;
+        public const int PLAYER_ADDRESS_STORAGE_SIZE = 32;
+        public const int PLAYER_STORAGE_SIZE = 10;
+
+        // Default player ressources
+        public const byte DEFAULT_MANA = 1;
+
+        public const byte DEFAULT_HUMANS = 1;
+        public const byte DEFAULT_WATER = 0;
+        public const byte DEFAULT_FOOD = 0;
+        public const byte DEFAULT_WOOD = 0;
+        public const byte DEFAULT_STONE = 0;
+        public const byte DEFAULT_GOLD = 0;
+
+        public const byte FREE_MANA_PER_ROUND = 1;
+
+        public const int NB_MAX_UNBOUNDED_TILES = 32;
+
+        public const int DEFAULT_WINNING_CONDITION_GOLD = 10;
+        public const int DEFAULT_WINNING_CONDITION_HUMAN = 7;
+
+        /// <summary>
+        /// Map tile upgrade cost
+        /// </summary>
+        /// <param name="tileType"></param>
+        /// <param name="tileLevel"></param>
+        /// <returns></returns>
+        public static byte[]? MapTileUpgradeCost(TileType tileType, byte tileLevel)
+        {
+            var materialCost = new byte[Enum.GetValues(typeof(RessourceType)).Length];
+
+            switch (tileType)
+            {
+                case TileType.Grass:
+                case TileType.Water:
+                case TileType.Mountain:
+                case TileType.Tree:
+                case TileType.Desert:
+                case TileType.Cave:
+                    return null;
+
+                case TileType.Home:
+                    materialCost[(int)RessourceType.Wood] = (byte)((tileLevel + 1) * 2);
+                    materialCost[(int)RessourceType.Stone] = (byte)((tileLevel + 1) * 2);
+                    materialCost[(int)RessourceType.Gold] = (byte)(tileLevel * 2);
+                    return materialCost;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Map tile cost
+        /// </summary>
+        /// <param name="tileType"></param>
+        /// <param name="tileLevel"></param>
+        /// <returns></returns>
+        public static byte[]? MapTileCost(TileType tileType, byte tileLevel)
+        {
+            var materialCost = new byte[Enum.GetValues(typeof(RessourceType)).Length];
+
+            switch (tileType)
+            {
+                case TileType.Grass:
+                case TileType.Water:
+                case TileType.Mountain:
+                case TileType.Tree:
+                case TileType.Desert:
+                case TileType.Cave:
+                    materialCost[(int)RessourceType.Mana] = (byte)(1 + tileLevel);
+                    return materialCost;
+
+                case TileType.Home:
+                    return null;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Tile offer
+        /// </summary>
+        public static TileOffer[] TILE_COSTS = new TileOffer[15] {
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Grass, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Grass, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Grass, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Water, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Water, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Water, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Mountain, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+             new TileOffer {
+                TileToBuy = new HexaTile(TileType.Mountain, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+              new TileOffer {
+                TileToBuy = new HexaTile(TileType.Mountain, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Tree, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Tree, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Tree, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Desert, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Cave, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+            new TileOffer {
+                TileToBuy = new HexaTile(TileType.Cave, 0, TilePattern.Normal),
+                SelectCost = new MaterialCost { MaterialType = RessourceType.Mana, Cost = 1, }
+            },
+        };
+    }
+}
---
+++ Substrate.Hexalem.NET/HexaPlayer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Substrate.Hexalem.Engine
+{
+    public partial class HexaPlayer : IHexaBase, ICloneable
+    {
+        public static implicit operator byte[](HexaPlayer p) => p.Value;
+
+        public static implicit operator HexaPlayer(byte[] p) => new HexaPlayer(p);
+
+        public byte[] Id { get; set; } // AccountId32
+
+        public byte[] Value { get; set; }
+
+        public HexaPlayer(byte[] id) : this(id, new byte[GameConfig.PLAYER_STORAGE_SIZE])
+        {
+            Value = new byte[GameConfig.PLAYER_STORAGE_SIZE];
+            TargetGoal = TargetGoal.HumanThreshold;
+            TargetValue = GameConfig.DEFAULT_WINNING_CONDITION_HUMAN;
+        }
+
+        public HexaPlayer(byte[] id, byte[] value)
+        {
+            Id = id;
+            Value = value;
+        }
+
+        public byte this[RessourceType ressourceType]
+        {
+            get => Value[(int)ressourceType];
+            set => Value[(int)ressourceType] = value;
+        }
+
+        public void Init(uint blockNumber)
+        {
+            this[RessourceType.Mana] = GameConfig.DEFAULT_MANA;
+            this[RessourceType.Humans] = GameConfig.DEFAULT_HUMANS;
+            this[RessourceType.Water] = GameConfig.DEFAULT_WATER;
+            this[RessourceType.Food] = GameConfig.DEFAULT_FOOD;
+            this[RessourceType.Wood] = GameConfig.DEFAULT_WOOD;
+            this[RessourceType.Stone] = GameConfig.DEFAULT_STONE;
+            this[RessourceType.Gold] = GameConfig.DEFAULT_GOLD;
+        }
+
+        /// <summary>
+        /// Ha
+        /// </summary>
+        /// <returns></returns>
+        public bool HasReachedTargetGoal()
+        {
+            switch (TargetGoal)
+            {
+                case TargetGoal.HumanThreshold:
+                    return this[RessourceType.Humans] >= TargetValue;
+
+                case TargetGoal.GoldThreshold:
+                    return this[RessourceType.Gold] >= TargetValue;
+
+                default:
+                    return false;
+            }
+        }
+
+        public void NextRound(uint blockNumber)
+        {
+        }
+
+        public void PostMove(uint blockNumber)
+        {
+        }
+
+        public object Clone()
+        {
+            return new HexaPlayer((byte[])Id.Clone(), (byte[])Value.Clone());
+        }
+    }
+
+    public partial class HexaPlayer
+    {
+        public TargetGoal TargetGoal
+        {
+            get => (TargetGoal)Value[7];
+            set => Value[7] = (byte)value;
+        }
+
+        public byte TargetValue
+        {
+            get => Value[8];
+            set => Value[8] = value;
+        }
+
+        public TargetState TargetState
+        {
+            get => (TargetState)Value[9];
+            set => Value[9] = (byte)value;
+        }
+    }
+}
