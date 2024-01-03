59f57568078d30202891ca62a2fb3e87605f1ddc
cedric.decoster <cedric.decoster@gmail.com>
2024-01-03 13:04:10 +0100

    updated to most actual net extension

--- Substrate.Hexalem.NET.NetApiExt/NET/NetApiExt/Generated/Model/pallet_hexalem/pallet/EnumError.cs
+++ Substrate.Hexalem.NET.NetApiExt/NET/NetApiExt/Generated/Model/pallet_hexalem/pallet/EnumError.cs
@@ -24,7 +24,7 @@ namespace Substrate.Hexalem.NET.NetApiExt.Generated.Model.pallet_hexalem.pallet
         
         HexBoardNotInitialized = 2,
         
-        CreatorNotInPlayersAtIndexZero = 3,
+        GameAlreadyStarted = 3,
         
         GameAlreadyCreated = 4,
         
